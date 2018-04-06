550ee76cb8fea4f58f7b03c62fa03bbcd7b169c0
Pawel Mstowski <pawel.mstowski@gmail.com>
2018-04-06 17:45:13 +0200

    delete debug

--- Debug/src_Game.cpp.o.d
+++
@@ -1,3 +0,0 @@
-Debug/src_Game.cpp.o: src/Game.cpp src/Game.hpp
-
-src/Game.hpp:
--- Debug/src_main.cpp.o.d
+++
@@ -1,6 +0,0 @@
-Debug/src_main.cpp.o: src/main.cpp src/assetsAndDefinitions.h \
- src/Game.hpp
-
-src/assetsAndDefinitions.h:
-
-src/Game.hpp:
