d6ebe1a859967407ceea0ba9a46934a1ea910852
Pawel Mstowski <pawel.mstowski@gmail.com>
2018-04-06 21:24:05 +0200

    testmap

---
+++ assets/maps/dungeon.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="dungeon" tilewidth="16" tileheight="16" tilecount="100" columns="10" backgroundcolor="#00740900">
+ <image source="ForgottenDungeon.png" width="160" height="160"/>
+ <tile id="74">
+  <objectgroup draworder="index"/>
+ </tile>
+ <tile id="84">
+  <objectgroup draworder="index"/>
+ </tile>
+</tileset>
