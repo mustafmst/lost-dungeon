c408ee0cedcc85a1e5bb1924bd841d7b0ec5588c
Pawel Mstowski <pawel.mstowski@gmail.com>
2018-04-17 19:45:09 +0200

    temporary floor

--- src/Player.cpp
+++ src/Player.cpp
@@ -11,12 +11,47 @@ void ld::Player::Draw(sf::RenderWindow& window)
 	window.draw(_player);
 }
 
+void createFloor(b2World& world) {
+	b2BodyDef def;
+	def.position = b2Vec2(400/F_SCALE, 600/F_SCALE);
+	def.type = b2_staticBody; //b2_kinematicBody;
+	b2Body* body = std::move(world.CreateBody(&def));
+	
+	b2PolygonShape box1;
+	box1.SetAsBox(400,10);
+	b2FixtureDef box1Fix;
+	box1Fix.shape = &box1;
+	box1Fix.density = 1;
+	body->CreateFixture(&box1Fix);
+}
+
 void ld::Player::InitPhysics(b2World& world)
 {
 	b2BodyDef def;
 	def.position = b2Vec2(_player.getPosition().x/F_SCALE, _player.getPosition().y/F_SCALE);
-	def.type = b2_dynamicBody;
+	def.type = b2_dynamicBody;//b2_kinematicBody;
 	_playerBody = std::move(world.CreateBody(&def));
+	
+	b2PolygonShape box1;
+	box1.SetAsBox(8.f/F_SCALE,17.f/F_SCALE);
+	b2FixtureDef box1Fix;
+	box1Fix.shape = &box1;
+	box1Fix.density = 1;
+	_playerBody->CreateFixture(&box1Fix);
+	
+	//Floor for test purposes
+	b2BodyDef def2;
+	def2.position = b2Vec2(400/F_SCALE, 500/F_SCALE);
+	def2.type = b2_staticBody; //b2_kinematicBody;
+	b2Body* body = std::move(world.CreateBody(&def2));
+	
+	b2PolygonShape box2;
+	box2.SetAsBox(400/F_SCALE,10/F_SCALE);
+	b2FixtureDef box2Fix;
+	box2Fix.shape = &box2;
+	box2Fix.density = 1;
+	body->CreateFixture(&box2Fix);
+	 
 }
 void ld::Player::Init(sf::Texture& texture)
 {
